37d2fe1b5cd69fd15e2697f1d8609a384563b6a1
Ariel BASSO MADJOUKENG <ariel.bassomadjoukeng@unamur.be>
2024-11-30 12:07:26 +0100

    local_local_augmentation added

--- Augmentation_method.py
+++ Augmentation_method.py
@@ -77,8 +77,57 @@ def global_global_augmentation(image_batch, active_group):
     
     return torch.stack(first_batch), torch.stack(second_batch)
 
-       
 
+def local_local_augmentation(batch_image, active_groups):
+    
+    selected_transformations = get_transformations(active_groups)
+    transform_batch_1 = random_choose(selected_transformations)
+    transform_batch_2 = random_choose(selected_transformations)
+    image_batch = image_batch.to(device)
+    with torch.no_grad():
+        predicted_roi = RoI_model(image_batch)
+
+    batch_image = np.array(batch_image.cpu())
+    for index, image in enumerate(batch_image):
+        boxes = predicted_roi[index]['boxes'].cpu().numpy()
+        masks = predicted_roi[index]['masks'].cpu().numpy()
+        scores =predicted_roi[index]['scores'].cpu().numpy()
+        
+        for i, box in enumerate(boxes):
+            if scores[i] > 0.5:
+                x1, y1, x2, y2 = box.astype(int)
+                mask = masks[i, 0] > 0.50  # Obtention du masque
+                image_permuted = image.transpose(1, 2, 0) 
+                colored_object = image_permuted[y1:y2, x1:x2].copy()
+                colored_object = Image.fromarray((colored_object * 255).astype(np.uint8), mode='RGB')
+                colored_object_transformed_1 = transform_batch_1(colored_object)
+                colored_object_transformed_2 = transform_batch_1(colored_object)
+                colored_object_transformed_1 = np.array(colored_object_transformed_1) / 255.0
+                colored_object_transformed_2 = np.array(colored_object_transformed_2) / 255.0
+                colored_object_transformed_1 = Image.fromarray((colored_object_transformed_1 * 255).astype(np.uint8))
+                colored_object_transformed_2 = Image.fromarray((colored_object_transformed_2 * 255).astype(np.uint8))
+                colored_object_transformed_1 = colored_object_transformed_1.resize((x2 - x1, y2 - y1))
+                colored_object_transformed_2 = colored_object_transformed_2.resize((x2 - x1, y2 - y1))
+                colored_object_transformed_1 = np.array(colored_object_transformed_1) / 255.0
+                colored_object_transformed_2 = np.array(colored_object_transformed_2) / 255.0
+                image_permuted_1 = image_permuted.copy()
+                image_permuted_2 = image_permuted.copy()
+                mask_resized = mask[y1:y2, x1:x2]  
+                mask_resized = np.expand_dims(mask_resized, axis=-1)  
+                mask_resized = np.repeat(mask_resized, 3, axis=-1)  
+                image_permuted_1[y1:y2, x1:x2][mask_resized] = colored_object_transformed_1[mask_resized]
+                image_permuted_2[y1:y2, x1:x2][mask_resized] = colored_object_transformed_2[mask_resized]
+                image_clipped_1 = np.clip(image_permuted_1, 0, 1)
+                image_clipped_2 = np.clip(image_permuted_2, 0, 1)
+                image_to_save_1 = image_permuted_1.transpose(2, 0, 1) 
+                image_to_save_2 = image_permuted_2.transpose(2, 0, 1)                
+                first_batch.append(image_to_save_1)
+                second_batch.append(image_to_save_2)  
+    first_batch = np.array(first_batch)
+    second_batch = np.array(second_batch)
+    first_batch = torch.from_numpy(first_batch)
+    second_batch = torch.from_numpy(second_batch)
+    return first_batch, second_batch
 
 
         
