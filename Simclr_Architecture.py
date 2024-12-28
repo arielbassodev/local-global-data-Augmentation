4f65f2f0142648706e1eda3e10c948a8f172e395
Ariel BASSO MADJOUKENG <ariel.bassomadjoukeng@unamur.be>
2024-12-28 18:38:36 +0100

    cod

--- Simclr_Architecture.py
+++ Simclr_Architecture.py
@@ -4,7 +4,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 backbone = resnet50(pretrained=True).to('cpu')
+
 backbone = nn.Sequential(*list(backbone.children())[:-1])
+
 class HeadProjection(nn.Module):
     def __init__(self):
         super().__init__()
