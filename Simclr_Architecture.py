6cf52c8e5571f60c0641fbe99712f6d1f8d28786
Ariel BASSO MADJOUKENG <ariel.bassomadjoukeng@unamur.be>
2024-12-28 17:54:13 +0100

    cod

--- Simclr_Architecture.py
+++ Simclr_Architecture.py
@@ -5,25 +5,18 @@ import torch.nn.functional as F
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 backbone = resnet50(pretrained=True).to(device)
 backbone = nn.Sequential(*list(backbone.children())[:-1]) 
-class Identity(nn.Module):
-    def __init__(self):
-        super(Identity, self).__init__()
-
-    def forward(self, x):
-        return x
-
 class HeadProjection(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(2048, 400)
-        self.fc2 = nn.Linear(400,200)
+        self.fc1 = nn.Linear(2048, 200)
+        self.fc2 = nn.Linear(200,39)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return x
 
-Projection = HeadProjection().to(device)
+Projection = HeadProjection().to('cpu')
 
 class SIMCLR(nn.Module):
     def __init__(self,backbone,projection):
@@ -34,4 +27,5 @@ class SIMCLR(nn.Module):
     def forward(self,x):
         x=self.projection(self.backbone(x))
         return x
+simclr_model = SIMCLR(backbone,Projection)
 
